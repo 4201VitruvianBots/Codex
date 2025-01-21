acb6dbf472fffc63e8650b68befb716fb75f5500
Jonathan Dao <jonathandao0@gmail.com>
2025-01-21 14:36:47 -0800

    Fixed imports

--- src/main/java/org/team4201/codex/Robot.java
+++ src/main/java/org/team4201/codex/Robot.java
@@ -1,8 +1,8 @@
 package org.team4201.codex;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -21,7 +21,7 @@ public class Robot extends TimedRobot {
      */
     public Robot() {
         if (RobotBase.isReal()) {
-          DriverStation.reportError("Codex should not be run as robot code!");
+          DriverStation.reportError("Codex should not be run as robot code!", false);
           throw new RuntimeException("Codex is run as robot code!");
         }
     }
