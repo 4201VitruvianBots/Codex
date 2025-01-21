82708c3f91667219e42f1580ec8f2fdb10989443
Jonathan Dao <jonathandao0@gmail.com>
2025-01-20 23:26:22 -0800

    Update to 2025.0.7
    -Re-added WPILib robot features for testing with simulation

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # Codex
 Team 4201's Generic Robot Software Library
 
+## Versioning
+_Note: Not a hard rule at the moment, but just my thoughts_
+
+Versioning uses the following numbering pattern **YYYY.AA.BB**
+* **YYYY** will always be the FRC year the library is compatible with
+* **AA** will be the week in the FRC Season the change was made.
+  * TODO: What about off-season? Should event changes have their own marking/tag?
+* **BB** will just be an increment from the previous published version.
+
+## Testing
+* Unit tests can be tested by running `./gradlew test`
+* Testing of GUI elements can be tested using `./gradlew simulateJava`
+
 ## Development Notes
 * Maven artifacts are saved under the [releases/](releases) directory after running `./gradlew publish`.
 * To publish changes to the library, run `./gradlew publish` and push the updated files under `releases/` to GitHub.
---
+++ src/main/java/org/team4201/codex/Robot.java
@@ -0,0 +1,83 @@
+package org.team4201.codex;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import org.team4201.codex.simulation.FieldSim;
+
+/**
+ * This is NOT meant for running full robot code. It is meant for testing code that can't be easily unit tested
+ * (e.g. {@link Mechanism2d}, {@link Field2d})
+ *
+ */
+public class Robot extends TimedRobot {
+    FieldSim fieldSim = new FieldSim();
+    /**
+     * This function is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+    public Robot() {
+
+    }
+
+    /**
+     * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+     * that you want ran during disabled, autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+        // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+        // commands, running already-scheduled commands, removing finished or interrupted commands,
+        // and running subsystem periodic() methods.  This must be called from the robot's periodic
+        // block in order for anything in the Command-based framework to work.
+        CommandScheduler.getInstance().run();
+        fieldSim.simulationPeriodic();
+    }
+
+    /** This function is called once each time the robot enters Disabled mode. */
+    @Override
+    public void disabledInit() {}
+
+    @Override
+    public void disabledPeriodic() {}
+
+    /** This autonomous runs the autonomous command selected by your RobotContainer class. */
+    @Override
+    public void autonomousInit() {
+    }
+
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {}
+
+    @Override
+    public void teleopInit() {
+    }
+
+    /** This function is called periodically during operator control. */
+    @Override
+    public void teleopPeriodic() {}
+
+    @Override
+    public void testInit() {
+        // Cancels all running commands at the start of test mode.
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {
+    }
+
+    /** This function is called once when the robot is first started up. */
+    @Override
+    public void simulationInit() {}
+
+    /** This function is called periodically whilst in simulation. */
+    @Override
+    public void simulationPeriodic() {}
+}
